9be8bf05ceb6e1a6263b9fa039434a270574d314
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-08-14 21:13:59 +0100

    Amended app port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', function (req, res) {
     res.send('Bellas my Bunny! <3 <3 <3');
 });
 
-app.listen(3000, function() {
-    console.log('Magic is happening on port 3000!');
+app.listen(8080, function() {
+    console.log('Magic is happening on port 8080!');
 });
