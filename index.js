1213901968df485b3ade245d6fb9c9bfaa21dec8
MikeAlexMartinez <mike.alex.martinez@gmail.com>
2017-11-15 07:59:38 +0000

    Added resume project to serve from Do-node server

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
+const path = require('path'); 
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    console.log('request received!')
-    res.send('Bellas my Bunny! <3 <3 <3');
-});
+// this allows us to serve static files from our public folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('*', express.static(path.join(__dirname + '/public/templates/')));
 
 app.listen(8080, function() {
     console.log('Magic is happening on port 8080!');
